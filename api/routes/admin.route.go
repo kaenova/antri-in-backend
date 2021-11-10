536fdb7031a41d23adb0381415302b1ba134a449
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-11-10 23:17:12 +0700

    test middleware

--- api/routes/admin.route.go
+++ api/routes/admin.route.go
@@ -2,12 +2,19 @@ package routes
 
 import (
 	"antri-in-backend/api/controller"
+	"antri-in-backend/utils"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Admin(e *echo.Echo) *echo.Echo {
 	g := e.Group("/admin")
 	g.POST("", controller.AdminPost)
+
+	s := e.Group("/admin")
+	s.Use(middleware.JWTWithConfig(utils.JWTconfigAdmin))
+	s.GET("", func(c echo.Context) error { return c.String(http.StatusOK, "Bruh") })
 	return e
 }
