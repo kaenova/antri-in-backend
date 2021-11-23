5e6bba2c91daad45994c38a5464ccfc3ec579704
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-11-23 08:09:42 +0700

    Check admin is already added

--- db/init_data.go
+++ db/init_data.go
@@ -5,6 +5,7 @@ import (
 	"antri-in-backend/entity"
 	"antri-in-backend/utils"
 	"antri-in-backend/utils/errlogger"
+	"errors"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -13,21 +14,28 @@ import (
 func initData(db *gorm.DB) {
 	config := config.GetConfig()
 
-	// Create SuperUser account
-	password, err := utils.HashPassword(config.SuperUser.Password)
+	err := db.Where("email = ?", config.SuperUser.Email).First(&entity.Admin{}).Error
 	if err != nil {
-		errlogger.ErrFatalPanic(err)
-	}
-	su := entity.Admin{
-		ID:       uuid.New(),
-		Nama:     config.SuperUser.Email,
-		Email:    config.SuperUser.Email,
-		Password: password,
-		Role:     entity.ROLES_SUPER_USER,
-		IsActive: true,
-	}
-	if err := db.Create(&su).Error; err != nil {
-		errlogger.ErrFatalPanic(err)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			// Create SuperUser account
+			password, err := utils.HashPassword(config.SuperUser.Password)
+			if err != nil {
+				errlogger.ErrFatalPanic(err)
+			}
+			su := entity.Admin{
+				ID:       uuid.New(),
+				Nama:     config.SuperUser.Email,
+				Email:    config.SuperUser.Email,
+				Password: password,
+				Role:     entity.ROLES_SUPER_USER,
+				IsActive: true,
+			}
+			if err := db.Create(&su).Error; err != nil {
+				errlogger.ErrFatalPanic(err)
+			}
+		} else {
+			errlogger.ErrFatalPanic(err)
+		}
 	}
 
 }
